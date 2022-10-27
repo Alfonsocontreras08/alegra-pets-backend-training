24c8df756cee40263da01700aa96dc166ea74448
Alfonso Contreras <alfonso.contreras@alegra.com>
2022-10-27 14:41:29 -0500

    creacion de lambda update-pet

--- lambdas/pet/update/alegra-pets-lambda-update-pet.js
+++ lambdas/pet/update/alegra-pets-lambda-update-pet.js
@@ -1,31 +1,73 @@
+const TablePet = process.env.TABLA_NAME_PET;
+const TableEntity = process.env.TABLA_NAME_ENTITY;
 const S3BucketName = process.env.S3_Bucket_Name;
 const REGION = process.env.REGION || "us-east-1";
 const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
-
-
-
-
-
+const AWS = require('aws-sdk');
+const { v4:uuid } = require("uuid");
+const DynamoDB = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = async (event)=>{
-    const body = JSON.parse(event.body);
-    saveBodyS3(body);
+    const body = JSON.parse(event.body); //falta validar el body , solo parametros definidos
+    const {petId} = event.pathParameters; 
+    const entity = await searchEntity(body.entity_owner);
+    if(entity.Items.length==0){
+        return {
+            statusCode:400,
+            body:JSON.stringify({
+                ok:false,
+                body:"La Entidad No Se Encuentra Registrada"
+            })
+        }
+    }
 
+
+    const pet = await searchPet(petId)
+    if(pet.Items.length==0){
+        return {
+            statusCode:400,
+            body:JSON.stringify({
+                ok:false,
+                body:"El Animal No Existe"
+            })
+        }
+    }
+    await saveBodyS3(body);
+    await updatePet(body);
+    
     return {
         statusCode:200,
-        "body":{
+        body: JSON.stringify({
             ok:true,
-            msj:"todo Perfecto"
-        }
+            body:"Registrado Correctamente"
+        })
     }
 }
 
+async function updatePet(pet){
+    const { state, ...obj} = pet;
+    const params = {
+        TableName:TablePet,
+        KeyConditionExpression: 'id = :petId ',
+        ExpressionAttributeValues: {
+            ':petId': petId
+        },
+        Item: {
+            id:uuid(),
+            ...obj,
+            "state":"Sad"
+        }
+    }
+    return await DynamoDB.put(params).promise()
+    .catch(e=>{throw new Error("Error :"+e)});
+}
+
 async function saveBodyS3(body){
     try {
         const s3Client = new S3Client({ region: REGION });
         const bucketParams = {
           Bucket: S3BucketName,
-          Key: `${new Date().toISOString()}-update-entity.json`,
+          Key: `${new Date().toISOString()}-create-pet.json`,
           Body: JSON.stringify(body),
         }
         
@@ -43,28 +85,32 @@ async function saveBodyS3(body){
     
 }
 
-async function saveBodyS3(body){
-    try {
-        const s3Client = new S3Client({ region: REGION });
-        const bucketParams = {
-          Bucket: S3BucketName,
-          Key: `${new Date().toISOString()}-create-entity.json`,
-          Body: JSON.stringify(body),
-        }
-        
-        const data = await s3Client.send(new PutObjectCommand(bucketParams));
-        console.log(
-          "Successfully uploaded object: " +
-            bucketParams.Bucket +
-            "/" +
-            bucketParams.Key
-        );
-        return data;
-    } catch (e) {
-        throw Error("error, no se pudo guardar el body en s3: ",e.message);
-   }
+async function searchEntity(entityId){
     
+    const params = {
+        TableName:TableEntity,
+        KeyConditionExpression: 'id = :petId ',
+        ExpressionAttributeValues: {
+            ':petId': entityId
+        }
+    }
+    console.log(params,"params searchById");
+    return await DynamoDB.query(params).promise()
+    .catch(e=>{throw new Error("Error: "+e)});
+
 }
-/**
-    actualizar mascota,
- */
+
+async function searchPet(petid){
+    
+    const params = {
+        TableName:TablePet,
+        KeyConditionExpression: 'id = :petId ',
+        ExpressionAttributeValues: {
+            ':petId': petId
+        }
+    }
+    console.log(params,"params searchById");
+    return await DynamoDB.query(params).promise()
+    .catch(e=>{throw new Error("Error: "+e)});
+
+}
