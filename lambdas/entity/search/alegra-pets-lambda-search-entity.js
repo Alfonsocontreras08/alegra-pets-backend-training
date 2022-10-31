c2575fedf2a391ade6847b56411acd6d515f5b53
Alfonso Contreras <alfonso.contreras@alegra.com>
2022-10-31 10:55:58 -0500

    validar que no mande el body vacio cuando no exiten elementos

--- lambdas/entity/search/alegra-pets-lambda-search-entity.js
+++ lambdas/entity/search/alegra-pets-lambda-search-entity.js
@@ -5,7 +5,17 @@ const dynamoDB = new AWS.DynamoDB.DocumentClient();
 exports.handler = async (event)=>{
     
     const data = await searchAll();
-  
+    
+    if(!data.Items[0]){
+        return {
+            statusCode:400,
+            body:JSON.stringify({
+                ok:false,
+                body:"No Existen Entidades Aun"
+            })
+        }
+    }
+
     return {
         statusCode:200,
         body:JSON.stringify({
